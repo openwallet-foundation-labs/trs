f535defb1019ddccdc03e965decb8cb02b9a9363
seorim yun <hhyjs2r@gmail.com>
2025-08-13 19:17:24 +0900

    feat: add trs module

--- trs/src/resolver/resolver.module.ts
+++ trs/src/resolver/resolver.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
 import { ResolverController } from '@/resolver/resolver.controller';
 import { RegistryModule } from '@/registry/registry.module';
+import { TrsService } from './services/trs.service';
 
 @Module({
-  imports: [RegistryModule],
+  imports: [RegistryModule, HttpModule],
   controllers: [ResolverController],
-  providers: [],
-  exports: [],
+  providers: [TrsService],
+  exports: [TrsService],
 })
 export class ResolverModule {}
